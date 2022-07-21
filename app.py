1eeffdd25db890e050906103f03dcf26028b4334
Mary Goncalves <mary.goncalves@architect.io>
2022-07-21 15:58:50 -0400

    Add hot-reload

--- app.py
+++ app.py
@@ -1,6 +1,15 @@
 import os
 from flaskr import create_app
+from livereload import Server
 
 if __name__ == "__main__":
     app = create_app()
-    app.run(host="0.0.0.0", port=os.environ["FLASK_PORT"])
+    # app.debug = True
+
+    # server = Server(app.wsgi_app)
+    # def alert():
+    #   print('--> here')
+    # server.watch('flaskr/templates/blog/index.html', alert)
+    # server.serve(host="0.0.0.0", port=os.environ["FLASK_PORT"])
+
+    app.run(host="0.0.0.0", port=os.environ["FLASK_PORT"], use_reloader=True)
--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -25,10 +25,6 @@ def create_app(test_config=None):
     except OSError:
         pass
 
-    @app.route("/hello")
-    def hello():
-        return "Hello, World!"
-
     # register the database commands
     from flaskr import db
 

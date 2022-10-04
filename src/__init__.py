15ce6ac8145afd2e84266b37edaecc62b31df283
Zach <zachary.jansma@architect.io>
2022-10-04 07:21:42 -0700

    Stripping down to just API layer

--- src/__init__.py
+++ src/__init__.py
@@ -1,14 +1,12 @@
 from flask import Flask
-from flask import render_template
-from flask import redirect
-from flask import flash
-from flask import url_for
 from flask import request
 import os
 from time import sleep
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import create_engine
 from sqlalchemy.sql import text
+
+from flask import jsonify
 
 """Wait until database is initialized to start app""" 
 # Build connection string
@@ -42,15 +40,20 @@ class Item(db.Model):
     name = db.Column(db.String(80), unique=False, nullable=False)
     rating = db.Column(db.Integer, unique=False, nullable=False)
 
+    def __repr__(self):
+        return str({"name": self.name, "rating":self.rating})
+
 db.create_all()
 
 @app.route("/", methods=['GET', 'POST'])
+@app.route("/movies", methods=['GET', 'POST'])
+@app.route("/items", methods=['GET', 'POST'])
 def index():
     """Create new movie entry"""
     if request.method == "POST":
-        print(request.form)
-        name = request.form["formName"]
-        rating = int(request.form["forRating"])
+        data = request.get_json()
+        name = data['name']
+        rating = data['rating']
         error = None
 
         if not name:
@@ -61,20 +64,21 @@ def index():
             error = "Choose rating between 1-5"
 
         if error is not None:
-            flash(error)
+            return {'error': error}
         else:
+            # Write to database
             entry = Item(name=name, rating=rating)
             db.session.add(entry)
             db.session.commit()
-            return redirect(url_for('index'))
 
     """Grab all movies in db"""
     output = ''
     try:
         output = Item.query.all()
+        output = jsonify(eval(repr(output)))
     except Exception as e:
         header = '<h1>Something is broken.</h1>'
         error_text = "<p>Error:<br>" + str(e) + "</p>"
-        return header + error_text
+        return {'error': header + error_text}
 
-    return render_template('movies.html', posts=output)
+    return output

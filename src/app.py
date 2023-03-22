b586b0077937a0cb181cda45fafd7a23ec5e0fb2
mtgoncalves1 <mary.goncalves@architect.io>
2023-03-22 13:27:12 -0400

    Fix database issue

--- src/app.py
+++ src/app.py
@@ -9,8 +9,8 @@ app.register_blueprint(controller_bp)
 
 db.init_app(app)
 
-if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
-    
+with app.app_context():
+    db.create_all()
+
+if __name__ == "__main__":
     app.run(debug=True)
